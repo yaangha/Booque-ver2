a0dd23a400042e4799d59b46ff2e6e5509e57da0
Jihye-Ryu <limegrey876@gmail.com>
2023-02-06 16:55:00 +0900

    0206 jihye chat
    - 쪼끔 수정... 일단 /chat 창은 뜨긴 뜸

--- src/main/java/site/book/project/web/ChatController.java
+++ src/main/java/site/book/project/web/ChatController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 import site.book.project.domain.Chat;
 import site.book.project.domain.User;
 import site.book.project.dto.ChatReadDto;
+import site.book.project.dto.UserSecurityDto;
 import site.book.project.repository.ChatRepository;
 import site.book.project.service.ChatService;
 
@@ -49,20 +51,19 @@ public class ChatController {
  
     // 중고판매글에서 '채팅하기' 버튼 클릭시
     @GetMapping("/chat")
-    public String getWebSocketWithSockJs(Model model, HttpSession session, 
+    public String getWebSocketWithSockJs(@AuthenticationPrincipal UserSecurityDto userDto, Integer sellerId, Integer usedBookId, Model model, 
             @ModelAttribute("chat") Chat chat) throws IOException {
         
         //중고판매글 detail 화면에서 Chat화면에 전달해줄 parameter
-//        User buyer = (User) session.getAttribute("loginUser");
-//        Integer buyerId = buyer.getId();
-//        chat.setBuyerId(buyerId);
-//        
+        Integer buyerId = userDto.getId();
+        chat.setBuyerId(buyerId);
+        
 //        User seller = (User) session.getAttribute("firstConnectUser");
 //        Integer sellerId = seller.getId();
-//        chat.setSellerId(sellerId);
-//        
+        chat.setSellerId(sellerId);
+        
 //        Integer usedBookId = (Integer) session.getAttribute("usedBookId");
-//        chat.setUsedBookId(usedBookId);
+        chat.setUsedBookId(usedBookId);
         
         
         //이미 chat이 만들어져있는지 확인
