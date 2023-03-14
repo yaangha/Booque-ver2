4fb8cf98036a4e27199eda2c386d65ed9fd393e5
yaangha <113163657+yaangha@users.noreply.github.com>
2023-03-15 00:08:46 +0900

    Update BookCommentRestController.java

--- src/main/java/site/book/project/web/BookCommentRestController.java
+++ src/main/java/site/book/project/web/BookCommentRestController.java
@@ -25,16 +25,11 @@ import site.book.project.service.PostService;
 @RequiredArgsConstructor
 @RestController  // response.data
 public class BookCommentRestController {
- 
     // BookDetail 페이지에서 사용할 AJax  REST controller comment와 post 함께 사용함
-    
-    
-    
-    
+ 
     private final BookCommentService bookCommentService;
     private final PostService postService;
     
-  
     @PostMapping("/api/comment") // 한줄평 insert
     public ResponseEntity<Integer> registerComment(@RequestBody BookCommentRegisterDto dto,
             @AuthenticationPrincipal UserSecurityDto userSecurityDto){
@@ -42,83 +37,51 @@ public class BookCommentRestController {
         Integer userId = userSecurityDto.getId();
         Integer commentId = bookCommentService.create(dto, userId);
         
-        
         return ResponseEntity.ok(commentId);
     }
     
     @GetMapping("/api/comment/all/{bookId}")
     public ResponseEntity<List<BookCommentReadDto>> readAllCommentDesc(@PathVariable Integer bookId) {
-        
         List<BookCommentReadDto> list = bookCommentService.readComment(bookId);
         
         return ResponseEntity.ok(list);
-        
     }
     
     @GetMapping("/api/comment/allOrderLike/{bookId}")
     public ResponseEntity<List<BookCommentReadDto>> readAllComment(@PathVariable Integer bookId) {
-        
         List<BookCommentReadDto> list = bookCommentService.readLikeComment(bookId);
         
-        
         return ResponseEntity.ok(list);
         
     }
-    
-    
-    
-    // postRest 사용할 예정
-    // 먼저 repositoy에서 query랑 문장 만들고 실행 되는지 확인  완.
-    // service로 옮겨서 실행 완성.되면
-    // ajax(post.js)만들고 컨트롤러(여기) get방식으로 받음 
-    // DTO를 만들어야 함... 
-    // TODO
+ 
     @GetMapping("/api/post/all/{bookId}")
     public ResponseEntity<List<PostReadDto>> readPostDesc(@PathVariable Integer bookId){
-        // bookid 받고 거기에 있는 post를 최신순으로
         List<PostReadDto> list = postService.findDesc(bookId);
-        
-        log.info(" 포스트 최신 순{}",list);
-        
-        
+     
         return ResponseEntity.ok(list);
     }
     
-    // 높은 별점순
     @GetMapping("/api/post/scoreDesc/{bookId}")
     public ResponseEntity<List<PostReadDto>> readPostScoreDesc(@PathVariable Integer bookId){
-        // bookid 받고 거기에 있는 post를 최신순으로
         List<PostReadDto> list = postService.findScoreDesc(bookId);
-        
-        log.info(" 포스트 별점{}",list);
-        
+     
         return ResponseEntity.ok(list);
     }
-    
-    // 낮은 별점순
     
     @GetMapping("/api/post/score/{bookId}")
     public ResponseEntity<List<PostReadDto>> readPostScore(@PathVariable Integer bookId){
-        // bookid 받고 거기에 있는 post를 최신순으로
         List<PostReadDto> list = postService.findScore(bookId);
-        
-        log.info(" 포스트 별점{}",list);
-        
+     
         return ResponseEntity.ok(list);
     }
     
     // (하은)
     @GetMapping("/api/post/content/{postId}")
     public ResponseEntity<PostReadDto> readPostContent(@PathVariable Integer postId) {
-        log.info("하은 로그 찾기 postid={}", postId);
         Post post = postService.read(postId);
         PostReadDto dto = PostReadDto.fromEntity(post);
         
         return ResponseEntity.ok(dto);
     }
-    
-    
-    
-    
-    
 }
